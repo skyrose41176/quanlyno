03cc04cee1ad5164bc0108b1b15baf9c1e4ace1f
Phan Trong Tinh <phantrongtinh@vietbank.com.vn>
2024-02-26 08:07:29 +0700

    fix

--- pages/_app.js
+++ pages/_app.js
@@ -5,3 +5,16 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp;
+// import { ThemeProvider } from "@mui/material/styles";
+// import CssBaseline from "@mui/material/CssBaseline";
+
+// import theme from "../styles/theme";
+
+// export default function MyApp({ Component, pageProps }) {
+//   return (
+//     <ThemeProvider theme={theme}>
+//       <CssBaseline />
+//       <Component {...pageProps} />
+//     </ThemeProvider>
+//   );
+// }
