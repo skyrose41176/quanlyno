d05e96f5a01503dcd55511b975f5327a75dca39e
Phan Trong Tinh <phantrongtinh@vietbank.com.vn>
2024-02-26 08:12:52 +0700

    fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,20 @@
-import "tailwindcss/tailwind.css";
+// import "tailwindcss/tailwind.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
-// import { ThemeProvider } from "@mui/material/styles";
-// import CssBaseline from "@mui/material/CssBaseline";
-
-// import theme from "../styles/theme";
-
-// export default function MyApp({ Component, pageProps }) {
-//   return (
-//     <ThemeProvider theme={theme}>
-//       <CssBaseline />
-//       <Component {...pageProps} />
-//     </ThemeProvider>
-//   );
+// function MyApp({ Component, pageProps }) {
+//   return <Component {...pageProps} />;
 // }
+
+// export default MyApp;
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+import theme from "../styles/theme";
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
